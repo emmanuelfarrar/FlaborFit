df06e3d148019af5b8710e639903615bfd330dfd
Emmanuel Farrar <6923873+emmanuelfarrar@users.noreply.github.com>
2021-01-21 22:06:01 +0100

    Finished Video 37

--- app/src/main/java/com/efarrar/flaborfit/DetailsActivity.java
+++ app/src/main/java/com/efarrar/flaborfit/DetailsActivity.java
@@ -2,8 +2,10 @@ package com.efarrar.flaborfit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class DetailsActivity extends AppCompatActivity {
@@ -14,10 +16,27 @@ public class DetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_details);
 
         //connecting elements by id
-        TextView exerciseTitle = findViewById(R.id.exerciseTitle);
+        TextView exerciseText = findViewById(R.id.exerciseTitle);
         ImageView exerciseImg = findViewById(R.id.exerciseImage);
+        LinearLayout mainBG = (LinearLayout)findViewById(R.id.mainBG);
 
+        String exerciseTitle = getIntent().getStringExtra(MainActivity.EXTRA_ITEM_TITLE); //getting the title via the placeholder text
+        exerciseText.setText(exerciseTitle);        //setting the exerciseTitle as the exerciseText
 
+        /*
+        Using if/else statements to set title, image, and background color based on what button was clicked.
+         */
+
+        if (exerciseTitle.equalsIgnoreCase(MainActivity.EXERCISE_WEIGHTS)) {
+            exerciseImg.setImageDrawable(getResources().getDrawable(R.drawable.weight, getApplicationContext().getTheme()));
+            mainBG.setBackgroundColor(Color.parseColor("#2ca5f5"));
+        } else if (exerciseTitle.equalsIgnoreCase(MainActivity.EXERCISE_YOGA)) {
+            exerciseImg.setImageDrawable(getResources().getDrawable(R.drawable.lotus, getApplicationContext().getTheme()));
+            mainBG.setBackgroundColor(Color.parseColor("#916bcd"));
+        } else {
+            exerciseImg.setImageDrawable(getResources().getDrawable(R.drawable.heart, getApplicationContext().getTheme()));
+            mainBG.setBackgroundColor(Color.parseColor("#52ad56"));
+        }
 
     }
 }
